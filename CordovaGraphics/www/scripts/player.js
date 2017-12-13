1805e103e03d11045668c3980025875c6ca09a0d
Aniiram <up201307722@fe.up.pt>
2017-12-13 11:48:03 +0100

    Player Shooting, moving

--- CordovaGraphics/www/scripts/player.js
+++ CordovaGraphics/www/scripts/player.js
@@ -1,18 +1,44 @@
-﻿
-  
-function drawPlayer() {
-    var playerX = 200;
-    var playerY = 490;
-    var playerWidth = 100;
-    var playerHeight = 10;
-    var playerSpeed = 10;    // number in range of 1 to 100, where 100 is fastest
-    var playerColour = "white";
-        // it is good coding practice to only include draw code inside draw functions
-        // and to not include drawing code anywhere else
-        ctx.fillStyle = playerColour;
-        ctx.fillRect(playerX, playerY, playerWidth, playerHeight);
-    }
+﻿function Player(life) {
+    this.life = life;
+    this.rotation = 180;
 
-    function setPlayerY(y) {
-        playerY = y;
-    }
+    this.towerWidth = canvas.width; 
+    this.towerHeight = canvas.height / 5;
+
+    this.playerWidth = 10;
+    this.playerHeight = 40;
+
+    this.y = canvas.height - canvas.height / 5 + this.playerHeight;
+
+    this.startPlayerX = - this.playerWidth / 2;
+    this.playerX = canvas.width / 2;
+    this.playerY = this.y + this.playerHeight;
+
+    // it is good coding practice to only include draw code inside draw functions
+    // and to not include drawing code anywhere else
+    this.drawPlayer = function () {
+        var towerColour = "black";
+        var playerColour = "red";
+
+        ctx.fillStyle = towerColour;
+        ctx.fillRect(0, this.y, this.towerWidth, this.towerHeight);
+
+        ctx.save();
+
+        ctx.translate(this.playerX, this.playerY);
+        ctx.fillStyle = playerColour;
+        ctx.rotate(this.rotation * Math.PI / 180);
+        ctx.fillRect(this.startPlayerX, 0, this.playerWidth, this.playerHeight);
+
+        ctx.restore();
+    };
+
+    this.gotShot = function (attack) {
+        this.life -= attack;
+
+        if (this.life <= 0) {
+            gameState = GAMESTATE_LOST;
+        }
+    };
+    
+}
