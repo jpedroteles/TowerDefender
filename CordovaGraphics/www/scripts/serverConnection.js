d631953f53906f69031db186b4b68a93bcd2b83d
Aniiram <up201307722@fe.up.pt>
2018-01-04 20:40:46 +0100

    Few details added

--- CordovaGraphics/www/scripts/serverConnection.js
+++ CordovaGraphics/www/scripts/serverConnection.js
@@ -2,8 +2,8 @@
 
 function startServer() {
     var socket = io('http://10.5.186.163:3000');
-
-    socket.on('news', function (data) {
+    socket.emit('chat message', 'Connected')
+    socket.on('chat message', function (data) {
         console.log(data);
         socket.emit('my other event', { my: 'data' });
     });
