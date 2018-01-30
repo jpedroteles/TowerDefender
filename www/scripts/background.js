e74117696336800c3b0a2a4c671cbdfe9398d2b2
Aniiram <up201307722@fe.up.pt>
2018-01-30 03:43:46 +0100

    final project

---
+++ www/scripts/background.js
@@ -0,0 +1,26 @@
+﻿var backgroundImgChoose = 1;
+var background = null;
+
+function backgroundImgChange(num) {
+    backgroundImgChoose += num;
+    
+    var mybackground = document.getElementsByClassName("myBackground");
+
+    if (backgroundImgChoose >= mybackground.length) {
+        backgroundImgChoose = 0;
+    }
+
+    if (backgroundImgChoose < 0) {
+        backgroundImgChoose = mybackground.length -1;
+    }
+
+    console.log(backgroundImgChoose);
+
+    for (var i = 0; i < mybackground.length; i++) {
+        if (backgroundImgChoose === i) {
+            mybackground[i].style.display = "block";
+        } else {
+            mybackground[i].style.display = "none";
+        }
+    }
+}
---
+++ www/scripts/sound.js
@@ -0,0 +1,40 @@
+﻿var playSound = true;
+var playAfterResume = false;
+
+// music
+var gameSound = document.createElement('audio');
+gameSound.src = 'music/adventuring_song.mp3';
+gameSound.loop = true;
+var menuSound = document.createElement('audio');
+menuSound.src = 'music/sketch4.mp3';
+menuSound.loop = true;
+var waitSound = document.createElement('audio');
+waitSound.src = 'music/glitch.mp3';
+waitSound.loop = true;
+
+function startSound() {
+    playSound = true;
+
+    if (gameState === GAMESTATE_PLAYING) {
+        gameSound.play();
+    }
+
+    if (gameState === GAMESTATE_LOST) {
+        menuSound.play();
+    }
+
+    if (gameState === GAMESTATE_WAIT) {
+        waitSound.play();
+    }
+}
+
+function muteSound() {
+    playSound = false;
+
+    menuSound.pause();
+    gameSound.pause();
+    waitSound.pause();
+    for (var i = 0; i < explosions.length; i++) {
+        explosions[i].explosionSound.pause();
+    }
+}
